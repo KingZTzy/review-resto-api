dc122fa3544ae2c2bf4a8e9f53ee40409fd0f954
KingZTzy <kenazsambur@gmail.com>
2023-02-02 08:29:30 +0700

    feat: Resto

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\RestoController;
 
 Route::post('/auth/login', [AuthenticationController::class, 'login'])->name('auth.login');
 Route::post('/auth/register', [AuthenticationController::class, 'register'])->name('auth.register');
@@ -11,3 +12,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/auth/profile', [AuthenticationController::class, 'profile'])->name('auth.profile');
     Route::get('/auth/logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
 });
+
+Route::apiResource('restos', RestoController::class);
